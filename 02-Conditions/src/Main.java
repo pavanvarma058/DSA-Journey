0fbd418e70ff42523ba2efe0790d17b40bbc7e11
Pavan Varma <pavanvarma058@gmail.com>
2025-12-16 15:09:48 +0000

    Learned about conditions in java

---
+++ 02-Conditions/src/Main.java
@@ -0,0 +1,11 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+void main() {
+    int a = 15;
+    int b = 8;
+    if(a > b){
+        System.out.println("a is largest number");
+    }else {
+        System.out.println("b is largest number");
+    }
+}
