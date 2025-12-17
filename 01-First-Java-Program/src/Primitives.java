1da2a36f85c2ec9dc6d623fdc53823edf2f56150
Pavan Varma <pavanvarma058@gmail.com>
2025-12-17 23:02:15 +0000

    Learned about primitives in java

---
+++ 01-First-Java-Program/src/Primitives.java
@@ -0,0 +1,13 @@
+public class Primitives {
+    static void main() {
+        int rollNo = 64;
+        char letter = 's';
+        float marks = 98.68f;
+        double largeDecimalNumber = 4567654.4567;
+        long largeInteger = 345678923L;
+        boolean check = true;
+
+        System.out.println(rollNo);
+        System.out.println(largeInteger);
+    }
+}
